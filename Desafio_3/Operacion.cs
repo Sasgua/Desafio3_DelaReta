87e148140cb9d2e6111f5a35e8dd47422bb38a7b
Valentin De la Reta <valentindelareta23@gmail.com>
2020-10-05 18:54:21 -0300

    Desafio numero 3

---
+++ Desafio_3/Operacion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_3
+{
+    class Operacion
+    {
+        public static void Suma(int x, int y)
+        {
+            int suma;
+
+            suma = x + y;
+            Console.WriteLine("La suma de los dos numeros es: " + suma);
+
+        }
+
+        public static void Resta(int x, int y)
+        {
+            int resta;
+
+            resta = x - y;
+            
+            Console.WriteLine("La resta de los numeros es: " + resta);
+            
+            
+            
+        }
+
+            
+
+    }
+}
---
+++ Desafio_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.WriteLine("Ingrese un numero");
+            Console.WriteLine("-Solo pueden ser enteros-");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("-Solo pueden ser enteros-");
+
+
+            
+            Operacion.Suma(n1, n2);
+            Operacion.Resta(n1, n2);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
